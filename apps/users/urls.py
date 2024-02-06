77ae46f9e85522a31c54be875992a73179c8e00e
niolum <Al_belov98@mail.ru>
2024-02-06 20:07:18 +0700

    add refresh token

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path, include
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path
+from rest_framework_simplejwt import views as jwt_views
 
 from apps.users.views import (
     RegistryUserView,
@@ -14,6 +14,7 @@ app_name = "users"
 
 urlpatterns = [
     path('register/', RegistryUserView.as_view(), name='register'),
+    path('refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('login/', UserLoginAPIView.as_view(), name='login-user'),
     path('logout/', UserLogoutAPIView.as_view(), name='logout-user'),
     path('user/', UserAPIView.as_view(), name='user-info'),
